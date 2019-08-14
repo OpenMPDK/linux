b50ff961b2d511b4fa8e0b33d7507883366f02ce
Javier González <javier.gonz@samsung.com>
2019-08-14 14:36:56 +0200

    nvme: add initial support for ZNS in zoned block
       - TP: 2019.10.30
    
    Signed-off-by: Javier González <javier.gonz@samsung.com>

--- drivers/nvme/host/nvme.h
+++ drivers/nvme/host/nvme.h
@@ -223,6 +223,7 @@ struct nvme_ctrl {
 	u64 cap;
 	u32 page_size;
 	u32 max_hw_sectors;
+	u32 max_zone_append_sectors;
 	u32 max_segments;
 	u16 crdt[3];
 	u16 oncs;
@@ -386,6 +387,14 @@ struct nvme_ns {
 	struct nvme_fault_inject fault_inject;
 
 	unsigned type;
+
+#ifdef CONFIG_BLK_DEV_ZONED
+	struct blk_zone *zones;
+	sector_t zone_secs;
+	sector_t zds;
+	u64 nr_zones;
+	bool is_zoned;
+#endif
 };
 
 struct nvme_ctrl_ops {
@@ -432,6 +441,22 @@ static inline u64 nvme_block_nr(struct nvme_ns *ns, sector_t sector)
 	return (sector >> (ns->lba_shift - 9));
 }
 
+static inline sector_t nvme_lbad_to_sec(struct nvme_ns *ns, sector_t sector)
+{
+	return (sector << (ns->lba_shift - 9));
+}
+#ifdef CONFIG_BLK_DEV_ZONED
+static inline void nvme_update_req(struct request *req,
+					union nvme_result result)
+{
+	struct nvme_ns *ns = req->q->queuedata;
+
+	if (rq_is_zone_append(req))
+		req->returned_sector =
+			nvme_lbad_to_sec(ns, (sector_t)le64_to_cpu(result.u64));
+
+}
+#endif
 static inline void nvme_end_request(struct request *req, __le16 status,
 		union nvme_result result)
 {
@@ -439,6 +464,10 @@ static inline void nvme_end_request(struct request *req, __le16 status,
 
 	rq->status = le16_to_cpu(status) >> 1;
 	rq->result = result;
+#ifdef CONFIG_BLK_DEV_ZONED
+	/* Set the returned sector for zone append commands */
+	nvme_update_req(req, result);
+#endif
 	/* inject error when permitted by fault injection framework */
 	nvme_should_fail(req);
 	blk_mq_complete_request(req);
