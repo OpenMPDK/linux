e64c6eef1e4d406d2ce8bd43302dabc6f9290075
SelvaKumar S <selvakuma.s1@samsung.com>
2020-08-26 17:25:00 +0530

    scc: change copy_ranges size from short to int
    Signed-off-by: SelvaKumar S <selvakuma.s1@samsung.com>

--- include/linux/blk_types.h
+++ include/linux/blk_types.h
@@ -194,7 +194,7 @@ struct bio {
 	blk_status_t		bi_status;
 	u8			bi_partno;
 	atomic_t		__bi_remaining;
-	unsigned short		bi_copy_ranges;
+	unsigned int		bi_copy_ranges;
 
 	struct bvec_iter	bi_iter;
 
